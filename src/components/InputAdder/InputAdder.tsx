9351582e466e2ba16f8aa0bf970c1fafa2302171
Ben Grandin <bengrandin@hotmail.com>
2020-05-05 13:22:01 +0200

    InputAdder : bases

--- src/components/InputAdder/InputAdder.tsx
+++ src/components/InputAdder/InputAdder.tsx
@@ -1,17 +1,49 @@
-import React, { FC } from 'react';
+import React, { FC, useState } from 'react';
 import './InputAdder.scss';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
-import { faPlusCircle } from '@fortawesome/free-solid-svg-icons';
+import { faPlusCircle,faTimes } from '@fortawesome/free-solid-svg-icons';
+import { ListHeader } from '../ListHeader/ListHeader';
 
 export const InputAdder: FC<any> = ({ children }) => {
+	const [displayAddText, setDisplayAddText] = useState(true);
+	// const [displayAddInput, setDisplayAddInput] = useState();
+
 	return (
 		<div className={'input-adder'}>
-			<div className={'add-text'}>
-				<div className={'icon'}>
-					<FontAwesomeIcon icon={faPlusCircle} color={'#000000'}/>
+
+
+			<div className={'add-text'} onClick={() => {
+				console.log('click');
+				setDisplayAddText(false);
+			}}>
+
+				<div>
+					{displayAddText &&
+                    <div className={'icon'}>
+                        <FontAwesomeIcon icon={faPlusCircle} color={'#000000'}/>
+                    </div>
+					}
+					<ListHeader className={`text ${!displayAddText ? 'card' : ''}`}
+								onBlur={() => console.log('blur')}
+								titleValue={'Ajouter une carte'}
+								placeHolder={'Saisisser le nom de la carte'}/>
 				</div>
-				<div className={'text'}>{children}</div>
+
+
+				{
+					!displayAddText &&
+                    <div className={'add-input'}>
+                        {/*<form>*/}
+                            <button className={'button bg-green'}>{children}</button>
+                            <div className={'icon'}>
+                                <FontAwesomeIcon icon={faTimes} color={'#000000'}/>
+                            </div>
+                        {/*</form>*/}
+                    </div>
+				}
 			</div>
+
+
 		</div>
 	);
 };
--- src/components/ListHeader/ListHeader.tsx
+++ src/components/ListHeader/ListHeader.tsx
@@ -1,10 +1,12 @@
-import React, { FC, FormEvent, useState } from 'react';
+import React, { FC, useState } from 'react';
 import './ListHeader.scss';
 
 
 interface InputTitleProps {
 	titleValue: string;
 	className?: string;
+	placeHolder?: string;
+	onBlur?: () => void;
 }
 
 
@@ -13,7 +15,7 @@ interface InputTitleProps {
 	 - Change input style,
  */
 
-export const ListHeader: FC<InputTitleProps> = ({ titleValue, className = '' }) => {
+export const ListHeader: FC<InputTitleProps> = ({ titleValue, placeHolder, className = '', onBlur }) => {
 	const [tempTitle, setTempTitle] = useState(titleValue);
 	const [title, setTitle] = useState(titleValue);
 	const [showInput, setShowInput] = useState(false);
@@ -22,7 +24,14 @@ export const ListHeader: FC<InputTitleProps> = ({ titleValue, className = '' }) 
 		setShowInput(false);
 		setTitle(tempTitle);
 	};
-	
+
+	const onInputBlur = () => {
+		onSubmitForm();
+		if( onBlur ) {
+			onBlur();
+		}
+	};
+
 	const onInputChange: ({ target: { value } }: { target: { value: string } }) => void = ({ target: { value } }) => {
 		setTempTitle(value);
 	};
@@ -36,16 +45,17 @@ export const ListHeader: FC<InputTitleProps> = ({ titleValue, className = '' }) 
 	};
 
 	return <div className={`list-header ${className}`}>
-		{ !showInput && <h4 className="list-title" onClick={()=>setShowInput(true)}>{title}</h4>}
+		{ !showInput && <h4 className="list-title" onClick={() => setShowInput(true)}>{title}</h4>}
 
 		{
 			showInput &&
             <form onSubmit={onSubmitForm}>
                 <input className='list-title'
                        value={tempTitle}
+                       placeholder={placeHolder}
                        onChange={onInputChange}
                        autoFocus
-                       onBlur={onSubmitForm}
+                       onBlur={onInputBlur}
                        onKeyDown={onEscapeInput}
                 />
             </form>
